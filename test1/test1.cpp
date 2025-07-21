0b217b8e137cc2b1fe404d90dfe2dd501b876609
seungboha <sha1307@gmail.com>
2025-07-21 18:04:43 +0900

    first commit

---
+++ test1/test1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdint>
+
+using std::uint64_t;
+
+
+static uint64_t Solution(uint64_t N);
+
+
+int main()
+{
+	uint64_t N = 0;
+
+	std::cin >> N;
+	std::cout << Solution(N) << std::endl;
+
+	return 0;
+}
+//========//========//========//========//=======#//========//========//========//========//=======#
+
+
+uint64_t Solution(uint64_t N)
+{
+	return 0;
+}
